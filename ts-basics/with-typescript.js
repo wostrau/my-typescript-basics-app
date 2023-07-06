c88a4cc26f0d1c5b3712cf385ff9792a620d614a
wostrau <a.ostrovtsov@gmail.com>
2023-07-06 16:19:22 +0200

    #467 - DONE

---
+++ ts-basics/with-typescript.js
@@ -0,0 +1,5 @@
+function addTs(a, b) {
+    return a + b;
+}
+var resultTs = addTs(2, 5);
+console.log(resultTs);
---
+++ ts-basics/with-typescript.ts
@@ -0,0 +1,7 @@
+function addTs(a: number, b: number): number {
+  return a + b;
+}
+
+const resultTs: number = addTs(2, 5);
+
+console.log(resultTs);
